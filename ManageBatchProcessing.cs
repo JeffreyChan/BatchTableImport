49cf9e042e4aba23c712ee37a1d81674655ca351
Jeffrey Chan <cdjboy@163.com>
2016-11-01 17:29:07 +0800

    batch data insert from server a to server b

---
+++ ManageBatchProcessing.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BatchTableImport
+{
+    public class ManageBatchProcessing
+    {
+        public string LocalConnStr { get; set; }
+        public string RemoteConnStr { get; set; }
+
+        public string CommandSql { get; set; }
+        public int BatchSize { get; set; }
+        public string TableName { get; set; }
+
+        public DataTable RetriveToDatabase(int item)
+        {
+            var start = (item - 1) * this.BatchSize + 1;
+            var end = item * this.BatchSize;
+            var dataTable = new DataTable();
+
+            using (var connection = new SqlConnection(this.RemoteConnStr))
+            {
+                var watch = new Stopwatch();
+                watch.Start();
+
+                using (var adapter = new SqlDataAdapter(string.Format(this.CommandSql, start, end), connection))
+                {
+                    adapter.SelectCommand.CommandTimeout = 3600;
+                    adapter.Fill(dataTable);
+                }
+
+                watch.Stop();
+
+                var totalSeconds = (double)watch.ElapsedMilliseconds / 1000;
+                Console.WriteLine("\t\t\t -------------------------------------------------");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\t\t\t convert datareader to table done {0} s", totalSeconds.ToString("#.##"));
+                Console.ResetColor();
+                Console.WriteLine("\t\t\t -------------------------------------------------");
+                return dataTable;
+            }
+        }
+
+        public void WriteToDatabase(DataTable dataTable)
+        {
+            using (var connection = new SqlConnection(this.LocalConnStr))
+            {
+                var watch = new Stopwatch();
+                watch.Start();
+
+                connection.Open();
+
+                using (var bulkCopy = new SqlBulkCopy(connection, SqlBulkCopyOptions.TableLock | SqlBulkCopyOptions.FireTriggers | SqlBulkCopyOptions.UseInternalTransaction, null))
+                {
+                    bulkCopy.DestinationTableName = this.TableName;
+                    bulkCopy.BulkCopyTimeout = 0;
+                    bulkCopy.WriteToServer(dataTable);
+                }
+
+                connection.Close();
+
+                watch.Stop();
+
+                var totalSeconds = (double)watch.ElapsedMilliseconds / 1000;
+                Console.WriteLine("\t\t\t -------------------------------------------------");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\t\t\t insert target table done {0} s", totalSeconds.ToString("#.##"));
+                Console.ResetColor();
+                Console.WriteLine("\t\t\t -------------------------------------------------");
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,169 @@
+﻿using Microsoft.FSharp.Collections;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BatchTableImport
+{
+    class Program
+    {
+        static BlockingCollection<DataTable> pcCollection = new BlockingCollection<DataTable>();
+        static string strRemoteConn = ConfigurationManager.AppSettings["RemoteConnStr"];
+        static string strLocalConn = ConfigurationManager.AppSettings["LocalConnStr"];
+        static string strCommandSql = ConfigurationManager.AppSettings["CommandSQL"];
+        static string strTableName = ConfigurationManager.AppSettings["TableName"];
+        static int batchSize = Int32.Parse(ConfigurationManager.AppSettings["CommitBatchSize"]);
+        static int taskCount = Int32.Parse(ConfigurationManager.AppSettings["TaskCount"]);
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                var watch = Stopwatch.StartNew();
+
+                var tableCount = 0D;
+
+                using (var connection = new SqlConnection(strRemoteConn))
+                using (SqlCommand cmd = connection.CreateCommand())
+                {
+                    connection.Open();
+                    cmd.CommandText = string.Format(@"SELECT
+                                                        Total_Rows= SUM(st.row_count)
+                                                    FROM
+                                                        sys.dm_db_partition_stats st
+                                                    WHERE
+                                                        object_name(object_id) = '{0}' AND (index_id < 2)", strTableName);
+                    cmd.CommandTimeout = 300;
+
+                    tableCount = Double.Parse(cmd.ExecuteScalar().ToString());
+                }
+
+                var totalPages = (int)Math.Ceiling(tableCount / batchSize);
+
+                var listPageRn = Enumerable.Range(1, totalPages);
+
+                var listPartPage = listPageRn.Split(taskCount).ToList();
+
+                var listProducerTask = new List<Task>();
+                var listConsumerTask = new List<Task>();
+
+                var consumerTask = taskCount / 2;
+
+                for (int i = 1; i <= consumerTask; i++)
+                {
+                    var taskFlag = i;
+                    var consumer = Task.Factory.StartNew(() =>
+                    {
+                        ConsumerAction(taskFlag.ToString());
+                    }, TaskCreationOptions.LongRunning);
+
+                    listConsumerTask.Add(consumer);
+                }
+
+                var producerTaskIndex = 1;
+                foreach (var item in listPartPage)
+                {
+                    var tmpIndex = producerTaskIndex.ToString();
+                    var producer = Task.Factory.StartNew(() =>
+                    {
+                        ProducerAction(item, tmpIndex);
+                    });
+
+                    listProducerTask.Add(producer);
+                    producerTaskIndex++;
+                }
+
+                Task.WaitAll(listProducerTask.ToArray());
+                pcCollection.CompleteAdding();
+
+                watch.Stop();
+                var mins = watch.ElapsedMilliseconds / 1000 / 60;
+                Console.WriteLine("All Batch Insert Time Elapsed:\t {0} mins", mins);
+
+                //Task.Factory.ContinueWhenAll(listProducerTask.ToArray(),
+                //     result =>
+                //     {
+                //         pcCollection.CompleteAdding();
+
+                //         watch.Stop();
+                //         var mins = watch.ElapsedMilliseconds / 1000 / 60;
+                //         Console.WriteLine("All Batch Insert Time Elapsed:\t {0} mins", mins);
+
+                //     });
+            }
+            catch (AggregateException ex)
+            {
+                using (StreamWriter writer = File.AppendText("BatchError.txt"))
+                {
+                    writer.WriteLine("Error Time: {0}", DateTime.Now);
+                    foreach (var exception in ex.InnerExceptions)
+                    {
+                        writer.WriteLine("Error: {0}", exception.Message);
+                        writer.WriteLine("Source: {0}", exception.Source);
+                        writer.WriteLine("Track: {0}", exception.StackTrace);
+                    }
+                }
+                throw;
+
+            }
+            catch (Exception ex)
+            {
+                using (StreamWriter writer = File.AppendText("BatchError.txt"))
+                {
+                    writer.WriteLine("Error Time: {0}", DateTime.Now);
+                    writer.WriteLine("Error: {0}", ex.Message);
+                    writer.WriteLine("Source: {0}", ex.Source);
+                    writer.WriteLine("Track: {0}", ex.StackTrace);
+                }
+                throw;
+            }
+
+            Console.ReadLine();
+        }
+
+        static void ProducerAction(IEnumerable<int> source, string taskFlag = "1")
+        {
+            foreach (var item in source)
+            {
+                Console.WriteLine("Producer-{0} processing item batch {1}", taskFlag, item);
+
+                var processing = new ManageBatchProcessing
+                {
+                    LocalConnStr = strLocalConn,
+                    RemoteConnStr = strRemoteConn,
+                    BatchSize = batchSize,
+                    TableName = strTableName,
+                    CommandSql = strCommandSql
+                };
+                pcCollection.Add(processing.RetriveToDatabase(item));
+            }
+        }
+
+        static void ConsumerAction(string taskFlag = "")
+        {
+            foreach (var item in pcCollection.GetConsumingEnumerable())
+            {
+                Console.WriteLine("consumer-{0} processing item {1}", taskFlag, item.Rows.Count);
+                var processing = new ManageBatchProcessing
+                {
+                    LocalConnStr = strLocalConn,
+                    RemoteConnStr = strRemoteConn,
+                    BatchSize = batchSize,
+                    TableName = strTableName,
+                    CommandSql = strCommandSql
+                };
+                processing.WriteToDatabase(item);
+            }
+        }
+
+    }
+}
